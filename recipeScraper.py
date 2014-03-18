0de92d49e5965e66513aab8409c04c42eca18b24
Grant Sheldon <gsheld@gmail.com>
2014-03-18 12:59:52 -0500

    Fixed xpath issue

--- recipeScraper.py
+++ recipeScraper.py
@@ -13,28 +13,28 @@ from itertools import cycle
 from pprint import pprint
 
 def getRecipeInfo():
-	
+
 	### Here the webpage with the recipe is opened ###
-	
+
 	driver = webdriver.Firefox()
 	myURL = 'http://allrecipes.com/Recipe/Slow-Cooker-Corned-Beef-and-Cabbage/'
 	print myURL
 	driver.get(myURL)
-	
+
 	### Here the recipe name is extracted ###
-	
+
 	recipeNameXPath = '//div[@class="detail-right fl-right"]/h1[@id="itemTitle"]'
 	recipeNameObject = driver.find_elements_by_xpath(recipeNameXPath)
-	
+
 	for value in recipeNameObject:
 		recipeName = value.get_attribute("innerHTML")
 	print recipeName
-	
+
 	ingredients = []
 	singleIngredient = {}
 	ingredientSet1NamesXPath = '//div[@class="ingred-left"]/ul[@class="ingredient-wrap"]/li[@id="liIngredient"]/label/p[@class="fl-ing"]/span[@id="lblIngName"]'
 	ingredientSet1NamesObjects = driver.find_elements_by_xpath(ingredientSet1NamesXPath)
-	
+
 	ingredientSet1AmountsXPath = '//div[@class="ingred-left"]/ul[@class="ingredient-wrap"]/li[@id="liIngredient"]/label/p[@class="fl-ing"]/span[@id="lblIngAmount"]'
 	ingredientSet1AmountsObjects = driver.find_elements_by_xpath(ingredientSet1AmountsXPath)
 
@@ -46,10 +46,10 @@ def getRecipeInfo():
 	for value in ingredientSet1AmountsObjects:
 		ingredients[i]['quantity'] = str(value.get_attribute("innerHTML"))
 		i += 1
-	
+
 	ingredientSet2NamesXPath = '//div[@class="ingred-left"]/ul[@class="ingredient-wrap secondColumn"]/li[@id="liIngredient"]/label/p[@class="fl-ing"]/span[@id="lblIngName"]'
 	ingredientSet2NamesObjects = driver.find_elements_by_xpath(ingredientSet2NamesXPath)
-	
+
 	ingredientSet2AmountsXPath = '//div[@class="ingred-left"]/ul[@class="ingredient-wrap secondColumn"]/li[@id="liIngredient"]/label/p[@class="fl-ing"]/span[@id="lblIngAmount"]'
 	ingredientSet2AmountsObjects = driver.find_elements_by_xpath(ingredientSet2AmountsXPath)
 
@@ -57,30 +57,30 @@ def getRecipeInfo():
 		singleIngredient['name'] = str(value.get_attribute("innerHTML"))
 		ingredients.append(singleIngredient)
 		singleIngredient = {}
-	
+
 	for value in ingredientSet2AmountsObjects:
 		ingredients[i]['quantity'] = str(value.get_attribute("innerHTML"))
 		i += 1
-	
+
 	#pprint(ingredients)
-	
+
 	directions = []
 	i = 0
-	
+
 	### PROBLEM POINT - DOESN'T FETCH ANYTHING ###
-	
-	directionsXPath = '//div[@class="directions"]/div[@class="directLeft"]/ol/li/span[@id="plaincharacterwrap break"]'
+
+	directionsXPath = '//div[@class="directLeft"]/ol/li/span'
 	directionsObjects = driver.find_elements_by_xpath(directionsXPath)
-	
+
 	for value in directionsObjects:
 		print value.get_attribute("innerHTML")		### SHOULD CONTAIN COOKING DIRECTIONS
 		print 'hello'
 		directions[i] = str(value.get_attribute("innerHTML"))
 		i += 1
 	print directions
-	
+
 	### END OF PROBLEM POINT ###
-	
+
 	cookingMethods = {}
 	with open('/Users/arundhatijaswal/Documents/Q2/NLP/recipes/recipe_project/text_files/cookingMethods.txt', 'r') as f:
 		for line in f:
@@ -90,7 +90,7 @@ def getRecipeInfo():
 	with open('/Users/arundhatijaswal/Documents/Q2/NLP/recipes/recipe_project/text_files/cookingUtensils.txt', 'r') as f:
 		for line in f:
 			cookingUtensils[string.replace(line, '\n', '')] = True
-	#pprint(cookingUtensils)	
+	#pprint(cookingUtensils)
 	recipeCookingMethods = []
 	recipeCookingUtensils = []
 	for step in directions:
@@ -107,16 +107,16 @@ def getRecipeInfo():
 	recipe['ingredients'] = ingredients
 	recipe['cooking method'] = recipeCookingMethods
 	recipe['cooking tools'] = recipeCookingUtensils
-	
+
 	pprint(recipe)
-	
+
 	#pg_src = driver.page_source.encode("utf-8")
 	#time.sleep(1)
 	driver.quit()
-	
+
 	"""
 	### The HTML content (treated as a giant string) is split on the basis of tags < & > ###
-	
+
 	collector = re.split('>|<', pg_src)
 	my_iter = cycle(collector)
 	recipe = {}
@@ -129,9 +129,9 @@ def getRecipeInfo():
 	parent = ''
 	#for i in range(2):
 	my_iter.next()
-	
+
 	### Now we cycle through our giant list of strings to find ingredient substitutions ###
-	
+
 	for item in collector:
 		info = my_iter.next()
 		if "colspan" in item:
@@ -151,7 +151,7 @@ def getRecipeInfo():
 				#print "\n",
 				#raw_input("Press Enter to continue...")
 			if re.match('[^0-9]', temp) != None and re.match('[A-Z]+', temp) == None:
-				#print "\n", 
+				#print "\n",
 				#parent=",parent,'<<'
 				if len(parent) > 0:
 					if string.find(temp, '\xc2') > -1:
@@ -184,7 +184,7 @@ def getRecipeInfo():
 					valFlag += 1
 				#sys.stdout.write(temp)
 				#sys.stdout.write(' |')
-				#print 
+				#print
 				#j += 1
 			i += 1
 			#print item
@@ -192,13 +192,13 @@ def getRecipeInfo():
 			if "colspan" in item:
 				return substitutions
 	"""
-	
+
 
 ### The main function is just to call the function that does everything and gets the data ###
 
 def main():
 	getRecipeInfo()
-	
+
 
 if __name__ == "__main__":
 	main()
